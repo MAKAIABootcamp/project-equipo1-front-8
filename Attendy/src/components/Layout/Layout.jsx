6005e1a7f1b688d64291da91193e15d21f54c733
Gustavo Garcia <gustavogarciagithub@gmail.com>
2024-09-13 13:35:33 -0500

    creacion login y inicios de sesion

--- Attendy/src/components/Layout/Layout.jsx
+++ Attendy/src/components/Layout/Layout.jsx
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { logoutThunk } from "../../redux/auth/authSlice";
 
 const Layout = () => {
+  const dispatch = useDispatch();
+  const {isAuthenticated} = useSelector(store=>store.auth)
+
+  const handleLogout = () => dispatch(logoutThunk());
+
   return (
     <div>
+      {isAuthenticated && <button onClick={handleLogout}>Cerrar sesión</button>}
       Layout
       <Outlet />
     </div>
--- Attendy/src/router/AppRouter.jsx
+++ Attendy/src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import PhoneLogin from "../pages/PhoneLogin/PhoneLogin";
+import VerificationCode from "../pages/VerificationCode/VerificationCode";
+
 // import Feed from "../pages/Feed/Feed";
-// import Register from "../pages/Register/Register";
-// import Login from "../pages/Login/Login";
 // import PostDetails from "../pages/PostDetails/PostDetails";
 // import Profile from "../pages/Profile/Profile";
 
@@ -12,13 +14,14 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="phoneLogin" element={<PhoneLogin/> } />
+          <Route path="verificationCode/:phoneNumber" element={<VerificationCode />} />
           <Route path="register" element={<Register />} />
-          {/* <Route index element={<Feed />} /> */}
-          {/* <Route path="post/:postId" element={<PostDetails />} />
-          <Route path="profile/:userId" element={<Profile />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<Navigate to={'/' } /> } /> */}
+          {/* <Route path="feed" element={<Feed />} /> */}
+          {/* <Route path="post/:postId" element={<PostDetails />} /> */}
+          {/* <Route path="profile/:userId" element={<Profile />} /> */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </BrowserRouter>
