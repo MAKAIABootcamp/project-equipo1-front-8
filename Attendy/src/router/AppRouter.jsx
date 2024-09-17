37fd0de6b79b645b45c98617c50cc5d39f50366e
Jhonier Santana <jhonier_2504@hotmail.com>
2024-09-17 11:51:13 -0500

    fix

--- Attendy/src/router/AppRouter.jsx
+++ Attendy/src/router/AppRouter.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
 import PhoneLogin from "../pages/PhoneLogin/PhoneLogin";
 import VerificationCode from "../pages/VerificationCode/VerificationCode";
+import Home from "../pages/Home/Home";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase/firebaseConfig";
@@ -40,15 +41,14 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/login" element={<Login />} />
-          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           <Route path="phoneLogin" element={<PhoneLogin />} />
           <Route
             path="verificationCode/:phoneNumber"
             element={<VerificationCode />}
           />
-          <Route path="register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
         </Route>
       </Routes>
     </BrowserRouter>
