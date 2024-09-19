8492ce0c977584f7621ac34eaf7d1786c6c1618f
Karen Herrera <134719336+karenjoha@users.noreply.github.com>
2024-09-19 11:19:35 -0500

    Si no cambiaba eso me daba un tic nervioso

--- Attendy/src/components/Footer.jsx
+++ Attendy/src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="bg-[#00A082] flex justify-between p-3 items-center">
         <div className="flex items-center">
             <p className="text-[#F2F2F2] font-poppins text-2xl">Attendy</p>
-            <img className="w-[50px]" src="/icons/logoblanco.svg" alt="Logo" />
+            <img className="w-[50px]" src="/icons/logoBlanco.svg" alt="Logo" />
         </div>
         <p className="text-[#F2F2F2]">© 2024 Attendy Inc. All rights reserved.</p>
     </div>
