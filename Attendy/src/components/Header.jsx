e2b78d63979aca015493259e5271063ce9e07dd3
Karen Herrera <134719336+karenjoha@users.noreply.github.com>
2024-09-24 09:21:25 -0500

    Completando vista home

--- Attendy/src/components/Header.jsx
+++ Attendy/src/components/Header.jsx
@@ -2,19 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { logoutThunk } from "../redux/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 const Header = () => {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const {isAuthenticated} = useSelector(store=>store.auth)
-  const handleClick = () => {
-    navigate('/login'); 
+  const handleClicklogin = () => {
+    navigate('/Login'); 
   };
-  const handleLogout = () => dispatch(logoutThunk());
   return (
     <div className="bg-[#E1E1E1] flex justify-between p-3">
         <div className="flex items-center">
             <p className="text-[#00A082] font-poppins text-2xl">Attendy</p>
             <img className="w-[50px]" src="/icons/logo.svg" alt="Logo" />
         </div>
-      <button className="bg-[#00A082] text-white px-5 rounded-2xl flex items-center" onClick={handleClick}>
+      <button className="bg-[#00A082] text-white px-5 rounded-2xl flex items-center" onClick={handleClicklogin}>
         <img className="w-[2rem]" src="/icons/user.svg" alt="" />
         Iniciar Sesion
         </button>
