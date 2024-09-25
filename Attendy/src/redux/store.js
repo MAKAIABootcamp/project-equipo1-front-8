07f070b717bb713a9461412a0a8df973bb414212
Fernanda Mantilla <fernandamantilla2016@gmail.com>
2024-09-25 11:23:32 -0500

    agregando la funcionalidad de la busqueda

--- Attendy/src/redux/store.js
+++ Attendy/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import searchReducer from "./search/searchSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    search: searchReducer,
   },
 });
 
