2b7ab561c83ec3dd98b2f85cb5352aae9424b2f5
Jhonier Santana <jhonier_2504@hotmail.com>
2024-09-30 21:02:56 -0500

    Update

--- Attendy/src/router/AppRouter.jsx
+++ Attendy/src/router/AppRouter.jsx
@@ -38,7 +38,7 @@ const AppRouter = () => {
             <h2 className=" text-[2rem] text-[#00A082]">Loading</h2>
             <svg
               aria-hidden="true"
-              class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-300 fill-[#00A082]"
+              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-300 fill-[#00A082]"
               viewBox="0 0 100 101"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
