2463afb79121f555b49f103f2cb92aad5ba069be
Jhonier Santana <jhonier_2504@hotmail.com>
2024-10-21 09:45:36 -0500

    Props

--- Attendy/src/router/PrivateRouter.jsx
+++ Attendy/src/router/PrivateRouter.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
 const PrivateRoute = ({ children, adminRoute = false }) => {
   const { user } = useSelector((state) => state.auth);
@@ -13,6 +14,11 @@ const PrivateRoute = ({ children, adminRoute = false }) => {
   }
 
   return children;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  adminRoute: PropTypes.bool,
 };
 
 export default PrivateRoute;
